8563641b9b6e44fab9fd511462c6db9f21754505
jamesmallet <jmallet@oeb.harvard.edu>
2025-12-09 15:57:18 -0500

    jim website

---
+++ javascript/hover.js
@@ -0,0 +1,14 @@
+function toggleDiv(id,flagit,s) {
+    if (flagit=="1"){
+        if (document.layers) document.layers[''+id+''].visibility = "show"
+        else if (document.all) document.all[''+id+''].style.visibility = "visible"
+        else if (document.getElementById) document.getElementById(''+id+'').style.visibility = "visible"
+        var objFrame=document.getElementById("myframe"); 
+        objFrame.src = s;
+    }
+    else if (flagit=="0"){
+        if (document.layers) document.layers[''+id+''].visibility = "hide"
+        else if (document.all) document.all[''+id+''].style.visibility = "hidden"
+        else if (document.getElementById) document.getElementById(''+id+'').style.visibility = "hidden"
+    }
+}
